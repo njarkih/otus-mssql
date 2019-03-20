eba25991eb3879cf389e556b38dc2769f260f264
njarkih <40836423+njarkih@users.noreply.github.com>
2019-03-20 22:30:43 +0300

    Update 6.sql

--- 2/6.sql
+++ 2/6.sql
@@ -1,13 +1,13 @@
 -- 6
--- Все ид и имена клиентов и их контактные телефоны, которые покупали товар Chocolate frogs 250g
+-- Р’СЃРµ РёРґ Рё РёРјРµРЅР° РєР»РёРµРЅС‚РѕРІ Рё РёС… РєРѕРЅС‚Р°РєС‚РЅС‹Рµ С‚РµР»РµС„РѕРЅС‹, РєРѕС‚РѕСЂС‹Рµ РїРѕРєСѓРїР°Р»Рё С‚РѕРІР°СЂ Chocolate frogs 250g
 SELECT 
   c.CustomerID
 , c.CustomerName
 , c.PhoneNumber 
 FROM Sales.Customers c
 WHERE EXISTS (SELECT 1 FROM Sales.Orders o
-			  JOIN Sales.OrderLines l ON o.CustomerID = c.CustomerID AND o.OrderID = l.OrderID
+	      JOIN Sales.OrderLines l ON o.CustomerID = c.CustomerID AND o.OrderID = l.OrderID
               JOIN Warehouse.StockItems i ON l.StockItemID = i.StockItemID
-			    AND i.StockItemName = 'Chocolate frogs 250g'
-			  )
+	        AND i.StockItemName = 'Chocolate frogs 250g'
+	       )
 ;
