48d18e393f1ef10349e18fa555f1eccb5165f1c6
njarkih <40836423+njarkih@users.noreply.github.com>
2019-03-24 18:02:46 +0300

    Create 2.sql

---
+++ 5/2.sql
@@ -0,0 +1,11 @@
+-- 2. Отобразить все месяцы, где общая сумма продаж превысила 10 000 
+SELECT 
+  YEAR(o.OrderDate) [Year]
+, MONTH(o.OrderDate) [Month]
+FROM Sales.Orders o
+JOIN Sales.OrderLines l ON o.OrderID = l.OrderID
+WHERE o.PickingCompletedWhen IS NOT NULL -- задана дата забора заказа
+GROUP BY YEAR(o.OrderDate), MONTH(o.OrderDate)
+HAVING SUM(l.Quantity * l.UnitPrice) > 10000
+ORDER BY [Year], [Month]
+;
