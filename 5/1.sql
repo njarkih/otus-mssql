860ead2a9f724506fccac04a94c5d40367813e93
njarkih <40836423+njarkih@users.noreply.github.com>
2019-03-24 18:01:45 +0300

    Create 1.sql

---
+++ 5/1.sql
@@ -0,0 +1,12 @@
+-- 1. Посчитать среднюю цену товара, общую сумму продажи по месяцам
+SELECT 
+  YEAR(o.OrderDate) [Year]
+, MONTH(o.OrderDate) [Month]
+, AVG(l.UnitPrice) AvgCost -- средняя цена товара 
+, SUM(l.Quantity * l.UnitPrice) FullCost -- общая сумма продажи
+FROM Sales.Orders o
+JOIN Sales.OrderLines l ON o.OrderID = l.OrderID
+WHERE o.PickingCompletedWhen IS NOT NULL -- задана дата забора заказа
+GROUP BY YEAR(o.OrderDate), MONTH(o.OrderDate)
+ORDER BY [Year], [Month]
+;
