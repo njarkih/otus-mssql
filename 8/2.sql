e273fe4b0f1824000a5a2592f01d67ad2ad57ab1
njarkih <40836423+njarkih@users.noreply.github.com>
2019-04-02 11:37:58 +0300

    Create 2.sql

---
+++ 8/2.sql
@@ -0,0 +1,21 @@
+--2. Для всех клиентов с именем, в котором есть Tailspin Toys
+--вывести все адреса, которые есть в таблице, в одной колонке
+
+--Пример результатов
+--CustomerName	AddressLine
+--Tailspin Toys (Head Office)	Shop 38
+--Tailspin Toys (Head Office)	1877 Mittal Road
+--Tailspin Toys (Head Office)	PO Box 8975
+--Tailspin Toys (Head Office)	Ribeiroville
+--.....
+
+SELECT CustomerName, AddressLine
+FROM
+   (SELECT CustomerName, DeliveryAddressLine1, DeliveryAddressLine2, PostalAddressLine1, PostalAddressLine2
+    FROM Sales.Customers
+    WHERE CustomerName LIKE 'Tailspin Toys%'
+   ) p
+UNPIVOT
+   (AddressLine FOR LineType IN (DeliveryAddressLine1, DeliveryAddressLine2, PostalAddressLine1, PostalAddressLine2)
+   ) AS unpvt
+;
