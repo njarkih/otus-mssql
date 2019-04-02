6c7c366ecb93a45292531125be3b55b96827c4e0
njarkih <40836423+njarkih@users.noreply.github.com>
2019-04-02 11:45:11 +0300

    Create 3.sql

---
+++ 8/3.sql
@@ -0,0 +1,26 @@
+-- 3. В таблице стран есть поля с кодом страны цифровым и буквенным
+-- сделайте выборку ИД страны, название, код - чтобы в поле был либо цифровой либо буквенный код
+-- Пример выдачи
+-- 
+-- CountryId	CountryName	Code
+-- 1	Afghanistan	AFG
+-- 1	Afghanistan	4
+-- 3	Albania	ALB
+-- 3	Albania	8
+
+SELECT 
+  CountryId
+, CountryName
+, Code
+FROM
+   (SELECT 
+      CountryId
+	, CountryName
+	, CONVERT(NVARCHAR(6), IsoAlpha3Code) IsoAlpha3Code
+	, CONVERT(NVARCHAR(6), IsoNumericCode) IsoNumericCode
+    FROM Application.Countries
+   ) p
+UNPIVOT
+   (Code FOR CodeType IN (IsoAlpha3Code, IsoNumericCode)
+   ) AS unpvt
+;
